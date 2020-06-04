e1e62dd3a9703d3456919f8c96a0bafece51cdd8
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-04 02:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
